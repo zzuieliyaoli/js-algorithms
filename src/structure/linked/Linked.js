0c6aeb5fcba1b9c471f1445421819be0ff05408a
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-09-24 01:00:38 +0800

    add iterator()

--- src/structure/linked/Linked.js
+++ src/structure/linked/Linked.js
@@ -3,12 +3,23 @@ const Node = require('./Node')
 class Linked {
   constructor() {
     this.first = new Node();
-    this.N = 1
+    this.N = 0
   }
   isEmpty() {
     return (this.first.next === null);
   }
   interator() {
+    let current = this.first
+    return {
+      hasNext: () =>( current.next !== null ),
+      next: () => {
+        let item = current.item
+        current = current.next
+        return item
+      },
+      remove: () => {
+      }
+    }
   }
   size() {
     return this.N
